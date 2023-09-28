828fab0ee16e449f6edf27bd928f393da3ec864b
Martinelli-89 <99336076+Martinelli-89@users.noreply.github.com>
2023-09-28 17:24:40 +0100

    added grid 4 multple users and card to add new one

--- frontend/src/Components/UserCard/UserCard.jsx
+++ frontend/src/Components/UserCard/UserCard.jsx
@@ -4,17 +4,20 @@ import male from "../../Assets/Img/male.svg";
 import female from "../../Assets/Img/female.svg";
 import update from "../../Assets/Img/update.svg";
 import bin from "../../Assets/Img/delete.svg";
+import addUser from "../../Assets/Img/addUser.svg";
 
 export const UserCard = (props) => {
+
+    const newUser = (props.newUser == undefined )? false : true;
 
     return (
 
         <section className="userCard">
-            <img src={female} alt="user icon" className="userIcon"/>
-            <h5 className="name">Alex Martinelli</h5>
-            <h6 className="title">Developer</h6>
-            <img className="update" src={update}></img>
-            <img className="delete" src={bin}></img>
+            <img src={!newUser? female : addUser} alt="user icon" className="userIcon"/>
+            <h5 className="name">{newUser? "Add user" : "Alex Martinelli"}</h5>
+            <h6 className="title">{newUser? "" : "Developer"}</h6>
+            {!newUser && <img className="update" src={update}></img>}
+            {!newUser && <img className="delete" src={bin}></img>}
         </section>
 
     )
--- frontend/src/Pages/Users/Users.jsx
+++ frontend/src/Pages/Users/Users.jsx
@@ -4,10 +4,30 @@ import { UserCard } from "../../Components/UserCard/UserCard";
 
 export const Users = () => {
 
+    const newUser = true;
+
     return (
 
         <section className="users">
-            <UserCard />
+            <div className="usersTitle">
+                <h3>Manager users</h3>
+            </div>
+            <section className="usersCards">
+                <UserCard />
+                <UserCard />
+                <UserCard />
+                <UserCard />
+                <UserCard />
+                <UserCard />
+                <UserCard />
+                <UserCard />
+                <UserCard />
+                <UserCard />
+                <UserCard />
+                <UserCard />
+                <UserCard />
+                <UserCard newUser={newUser}/>
+            </section>
         </section>
 
     )
